54c9039d08a512751df3edaa8937378f3c336c24
Vinícius Mota Zulianelli <70486496+ViniciusaiGP@users.noreply.github.com>
2023-09-05 09:32:35 -0300

    update-server

--- api/index.py
+++ api/index.py
@@ -1,10 +1,24 @@
-from http.server import BaseHTTPRequestHandler
+from flask import Flask, jsonify, request
+from flask_cors import CORS 
 
-class handler(BaseHTTPRequestHandler):
+app = Flask(__name__)
+CORS(app)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+@app.route('/test-post', methods=['POST'])
+def teste():
+    # Verifica se a requisição é do tipo JSON
+    if request.is_json:
+        data = request.get_json()
+        # Verifica se as chaves 'sobrenome' e 'nome' estão presentes nos dados recebidos
+        if 'sobrenome' in data and 'nome' in data:
+            nome = data['nome']
+            sobrenome = data['sobrenome']
+            # Faça o que quiser com o sobrenome e o nome aqui
+            return jsonify({'mensagem': f'Recebi  Nome: {nome} e sobrenome: {sobrenome}'})
+        else:
+            return jsonify({'erro': 'Dados incompletos'}), 400
+    else:
+        return jsonify({'erro': 'Requisição não é JSON'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
